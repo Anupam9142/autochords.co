642412860290181f2cac172ade8d269e6895303b
Anupam kumar <105556225+Anupam9142@users.noreply.github.com>
2024-12-09 10:47:16 +0000

    finally it done

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,64 @@
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+  } from "~/app/components/ui/dropdown-menu"
+  
+  import React from 'react'
+  import Image from "next/image"
+  import { getServerSession } from "next-auth";
+  import { SessionProvider, signIn } from "next-auth/react";
+  import Provider from "~/app/components/Provider";
+  import LoginButton from "./LoginButton";
+  import LogoutButton from "./LogoutButton";
+  
+  type Session = {
+    user: {
+      name: string;
+      email: string;
+      image: string;
+    }
+  } | null
+  
+  async function Header({showWordMark = true, className}: {showWordMark?: boolean; className?: string}) {
+    const session: Session = (await getServerSession()) ?? null;
+  
+    return (
+  
+      <div className={"absolute flex top-0 h-16 items-center justify-between pr-8 w-full z-50 " + className}>
+        <a href="/" className="flex items-center">
+          <Image className="mt-2" width={80} height={80} src="/logo.png" alt="" />
+          {showWordMark && <span className="text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-b from-white to-white/30">AutoChords</span>}
+        </a>
+        <DropdownMenu>
+          {session ? (
+            <DropdownMenuTrigger>
+              <div>
+                <div className="flex gap-2 items-center justify-center ">
+                  <img className="w-10 h-10 rounded-full" src={session.user.image} alt="" />
+                  <span className="text-sm font-semibold ">{session.user.name}</span>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-3 h-3 ">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                  </svg>
+  
+                </div>
+              </div>
+            </DropdownMenuTrigger>
+          ) : (
+              <LoginButton/>
+          )}
+  
+          <DropdownMenuContent >
+            <DropdownMenuItem >Key</DropdownMenuItem>
+            <DropdownMenuItem ><LogoutButton/></DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+  
+    )
+  }
+  
+  export default Header
---
+++ src/app/components/micaccess.tsx
@@ -0,0 +1,71 @@
+'use client';
+
+import React, { useState } from 'react'
+import { useSession } from 'next-auth/react';
+
+function MicAccess() {
+
+  const {data: session} = useSession();
+
+  const [isRecording, setIsRecording] = useState(false);
+  const [counter, setCounter] = useState(15);
+
+  const [key, setKey] = useState("");
+
+  const startRecording = () => {
+    setIsRecording(true);
+    void navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
+      const mediaRecorder = new MediaRecorder(stream);
+      mediaRecorder.start();
+      const audioChunks: BlobPart[] | undefined = [];
+      mediaRecorder.addEventListener("dataavailable", (event) => {
+        audioChunks.push(event.data);
+      });
+      mediaRecorder.addEventListener("stop", () => {
+        const audioBlob = new Blob(audioChunks);
+
+        setIsRecording(false);
+        setCounter(15);
+
+        // Send audio to API endpoint
+        const formData = new FormData();
+        formData.append('file', audioBlob, 'recording.wav');
+        fetch('/api/user_recording?user_email=' + session?.user?.email ?? 'guest' , {
+          method: 'POST',
+          body: formData
+        })
+          .then(response => {
+            console.log(response.json().then((data) => (data as {key: string})).then((data) => setKey(data.key)));
+            console.log('Audio sent to API endpoint');
+          })
+          .catch(error => {
+            console.error('Error sending audio to API endpoint:', error);
+          });
+      });
+      const interval = setInterval(() => {
+        setCounter((prevCounter) => prevCounter - 1);
+      }, 1000);
+      setTimeout(() => {
+        clearInterval(interval);
+        mediaRecorder.stop();
+      }, 15000);
+    });
+  };
+
+
+  return (
+    <div className={`flex flex-col gap-2 items-center justify-center font-bold`}>
+      <div className='text-white'>Your key: {key}</div>
+      <button
+        onClick={startRecording}
+        disabled={isRecording}
+        className={`w-1/4 h-8 rounded-full ${isRecording ? 'bg-red-500 text-white' : 'bg-white border border-red-500 text-red-500 hover:bg-red-500 hover:text-white'}`}
+      >
+        {isRecording ? `Recording... ${counter}` : 'Record'}
+      </button>
+    </div>
+  );
+
+}
+
+export default MicAccess
